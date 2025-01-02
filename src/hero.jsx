320ae9502699e25e23699ba7579d1aee26dac00e
Trivedi_Vishwajeet <125482211+kumarvishwajeettrivedi@users.noreply.github.com>
2025-01-02 10:43:35 +0530

    Update hero.jsx
    back to normal

--- src/hero.jsx
+++ src/hero.jsx
@@ -59,11 +59,11 @@ useEffect(() => {
     <section className="home-hero">
       {
     renderBlobs && (
-      <div className="blob-container">
+      <>
         <BlobCanvas />
         <BlobCanvas2 />
         <BlobCanvas3 />
-      <div/>
+      </>
     
   )}
       
@@ -90,11 +90,11 @@ useEffect(() => {
       <div className="home-hero__content">
       {
     renderBlobs && (
-      <div className="blob-container">
+      <>
         <BlobCanvas />
         <BlobCanvas2 />
         <BlobCanvas3 />
-      <div/>
+      </>
     
   )}
         
